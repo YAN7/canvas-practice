c934844bfc3bf7ba229d6738c234e868d56927b4
YAN7 <904394373@qq.com>
2023-11-14 12:31:10 +0800

    自动注册路由&自动生成菜单

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,46 @@
+<template>
+  <div class="main">
+    <el-menu style="width: 260px" router>
+      <el-menu-item v-for="btn in btns" :key="btn.path" :index="btn.path">
+        {{ btn.name }}
+      </el-menu-item>
+    </el-menu>
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+</template>
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import { RouterView } from 'vue-router'
+
+const btns = ref<{ path: string; name: string }[]>([])
+
+const getBtns = () => {
+  const modules = import.meta.glob('./views/*.vue')
+  const autoRoutes = Object.entries(modules).map(([key]) => {
+    const path = key.replace('./views', '').replace('.vue', '')
+    return {
+      path,
+      name: path.replace('/', '')
+    }
+  })
+  btns.value = autoRoutes
+}
+
+onMounted(() => {
+  getBtns()
+})
 </script>
 
-<template>
-  <RouterView />
-</template>
+<style scoped>
+.main {
+  display: flex;
+}
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+</style>
---
+++ src/views/first-canvas.vue
@@ -0,0 +1,18 @@
+<template>
+  <canvas ref="canvasRef"></canvas>
+</template>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const canvasRef = ref<HTMLCanvasElement>()
+
+onMounted(() => {
+  const ctx = canvasRef.value?.getContext('2d')!
+  ctx.fillStyle = 'cyan'
+  /**
+   * 画布默认是300px*150px
+   * 前两个参数是表示距离画布左上角的距离(x,y)
+   */
+  ctx.fillRect(10, 20, 200, 100)
+})
+</script>
+<style scoped lang="less"></style>
