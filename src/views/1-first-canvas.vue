e4a3fbb81074a0415c0b43b31dfe2875eeddedb4
YAN7 <904394373@qq.com>
2023-11-14 20:29:31 +0800

    优化1,2页面

--- src/views/1-first-canvas.vue
+++ src/views/1-first-canvas.vue
@@ -1,5 +1,13 @@
 <template>
-  <canvas ref="canvasRef" width="500" height="300"></canvas>
+  <div class="flex flex-1">
+    <div class="flex-1 p-[20px]" style="border-right: 1px solid #999">
+      <p>画布默认是300px*150px</p>
+      <p>坐标原点在左上角</p>
+    </div>
+    <div style="flex: 0 0 550px" class="flex justify-center mt-[20px]">
+      <div><canvas ref="canvasRef" width="500" height="500"></canvas></div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
--- src/views/2-two-canvas.vue
+++ src/views/2-two-canvas.vue
@@ -1,5 +1,13 @@
 <template>
-  <canvas ref="canvasRef" width="500" height="300"></canvas>
+  <div class="flex flex-1">
+    <div class="flex-1 p-[20px]" style="border-right: 1px solid #999">
+      <p>canvas的上下文通过getContext方法获取</p>
+      <p></p>
+    </div>
+    <div style="flex: 0 0 550px" class="flex justify-center mt-[20px]">
+      <div><canvas ref="canvasRef" width="500" height="500"></canvas></div>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 import { ref, onMounted, type Ref } from 'vue'
