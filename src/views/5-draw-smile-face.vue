41eccfb0ba7aeaf18cfecc029618ffcccdf95da4
YAN7 <904394373@qq.com>
2023-11-14 21:55:50 +0800

    绘制笑脸

--- src/views/5-draw-smile-face.vue
+++ src/views/5-draw-smile-face.vue
@@ -3,12 +3,13 @@
     <div class="flex-1 p-[20px]" style="border-right: 1px solid #999">
       <p>画图形的步骤:</p>
       <ol>
-        <li>beginPath起手</li>
-        <li>moveTo指定第一个点</li>
-        <li>lineTo是绘制直线的方法</li>
-        <li>使用closePath闭合图形</li>
-        <li>此时页面上还看不出来任何东西,需要用fill去填充</li>
-        <li>要新画一个图形的话,重新以beginPath起手</li>
+        <li>
+          arc是画圆的api,mdn文档地址:
+          https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasRenderingContext2D/arc
+        </li>
+        <li>
+          moveTo相当于提笔落点,就是相当于在写字的时候,写完一个字,需要提笔到另一个点写另一个字一样,中间不留下笔迹
+        </li>
       </ol>
     </div>
     <div style="flex: 0 0 550px" class="flex justify-center mt-[20px]">
@@ -28,19 +29,14 @@ onMounted(() => {
   if (ctx.value) {
     let cc = unref(ctx)!
     cc.beginPath()
-    cc.moveTo(200, 100)
-    cc.lineTo(200, 200)
-    cc.lineTo(250, 150)
-    cc.closePath()
-    cc.fillStyle = 'cyan'
-    cc.fill()
-    cc.beginPath()
-    cc.moveTo(190, 100)
-    cc.lineTo(190, 200)
-    cc.lineTo(140, 150)
-    cc.closePath()
-    cc.fillStyle = 'orange'
-    cc.fill()
+    cc.arc(75, 75, 50, 0, Math.PI * 2, true) // 绘制
+    cc.moveTo(110, 75)
+    cc.arc(75, 75, 35, 0, Math.PI, false) // 口 (顺时针)
+    cc.moveTo(65, 65)
+    cc.arc(60, 65, 5, 0, Math.PI * 2, true) // 左眼
+    cc.moveTo(95, 65)
+    cc.arc(90, 65, 5, 0, Math.PI * 2, true) // 右眼
+    cc.stroke()
   }
 })
 </script>
