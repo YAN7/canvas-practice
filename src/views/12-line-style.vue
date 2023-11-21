287c86dfd25378b7efcdef8e7d7a08802c0f31d1
YAN7 <904394373@qq.com>
2023-11-21 16:43:37 +0800

    line-style

--- src/views/12-line-style.vue
+++ src/views/12-line-style.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-1">
     <div class="flex-1 p-[20px]" style="border-right: 1px solid #999">
+      <h3>lineWidth, 设置线宽</h3>
       <ol>
-        <li>fillStyle的写法和css中的color写法一样,支持多种颜色定义方式</li>
+        <li>只能是正数</li>
       </ol>
     </div>
     <div style="flex: 0 0 550px" class="flex justify-center mt-[20px]">
       <div><canvas ref="canvasRef" width="500" height="500"></canvas></div>
-      <div>123</div>
     </div>
   </div>
 </template>
@@ -18,22 +18,31 @@ import { useCanvas } from '@/hooks/useCanvas'
 const canvasRef = ref<HTMLCanvasElement>()
 
 const { ctx } = useCanvas(canvasRef as Ref<HTMLCanvasElement>)
+
+const drawLineWidth = (cc: CanvasRenderingContext2D) => {
+  const colors = ['#acef11', '#9137ea', '#f50808', '#f80780', '#00b7ff']
+  cc.lineWidth = 1
+  cc.beginPath()
+  cc.strokeStyle = 'orange'
+  cc.strokeRect(5, 5, 250, 250)
+  for (let i = 1; i <= 5; i++) {
+    cc.beginPath()
+    cc.lineWidth = i
+    cc.strokeStyle = colors[i - 1]
+    cc.moveTo(50 * i - 20, 10)
+    cc.lineTo(50 * i - 20, 245)
+    cc.stroke()
+  }
+}
+
+const drawLineCap = (cc: CanvasRenderingContext2D) => {
+  const lineCapStyle = ['butt', 'round', 'square']
+}
+
 onMounted(() => {
   if (ctx.value) {
     const cc = unref(ctx)!
-
-    const colors = ['#acef11', '#9137ea', '#f50808', '#f80780', '#00b7ff']
-    cc.lineWidth = 1
-    cc.beginPath()
-    cc.strokeStyle = 'orange'
-    cc.strokeRect(5, 5, 50, 100)
-    for (let i = 1; i <= 5; i++) {
-      cc.beginPath()
-      cc.strokeStyle = colors[i - 1]
-      cc.moveTo(10 * i, 10)
-      cc.lineTo(10 * i, 100)
-      cc.stroke()
-    }
+    drawLineWidth(cc)
   }
 })
 </script>
